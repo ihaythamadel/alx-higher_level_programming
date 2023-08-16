b0d7028738f86b338284601ceb6a31b40ae13b83
ihaythamadel <ihaythamadel@gmail.com>
2023-08-16 14:04:59 +0000

    6-print_sorted_dictionary.py

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    new_dict = {key: a_dictionary[key] for key in sorted(a_dictionary.keys())}
+    for key, value in new_dict.items():
+        print(f"{key}: {value}")
