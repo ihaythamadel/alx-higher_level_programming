c665134a50400e20927de854f1347a48e25ac5e2
ihaythamadel <123040960+ihaythamadel@users.noreply.github.com>
2023-09-18 21:01:17 +0300

    models

---
+++ 0x0C-python-almost_a_circle/models/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""The __init__.py file is used to indicate that the directory
+it is in should be treated as a Python package.
+This file is typically empty, but it can be used to perform initialization
+tasks for the package, such as setting variables or defining submodules.
+This file is required for the package to be importable.
+And it is used by the import system to find and load the package when it is imported
+"""
